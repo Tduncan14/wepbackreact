d46d5e9bc3f5790644a2f8e6ac21ec3b85abd26b
Treek <nbatre1@yahoo.com>
2020-06-12 12:57:24 -0500

    keep moving forward

---
+++ app/Main.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+function Example(){
+
+    return(
+        <div>
+            <h1>
+                This the app
+
+
+            </h1>
+
+            <p> The is great</p>
+        </div>
+    )
+}
+
+
+ReactDOM.render(<Example/>, document.querySelector('#app'))
