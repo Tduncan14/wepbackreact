99a72d0474e1d360b6fcb643c6360cf6516d2c7e
Treek <nbatre1@yahoo.com>
2020-06-20 17:36:34 -0500

    keep moving forward

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,34 +1,78 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Container from './Container';
 import Page from './Page';
-const Main = () => (
+import axios from 'axios';
+
+const Main = () => {
+
+   const [username,setusername] = useState()
+
+   const [useremail,setuseremail] = useState()
 
 
-    <Page title =" Home " wide={true} >
+   const[password,setpassword] = useState()
+
+
+    // function username (e){
+
+
+    //   setusername(e.target.value)
+    // }
+
+
+     async function handleSubmit(e){
+
+      e.preventDefault()
+
+    try {
+      await axios.post('http://localhost:8080/register',{
+        username:username,
+        email:useremail,
+        password:password
+      })
+
+
+      console.log('user was created')
+
+     }
+
+
+     catch(e){
+
+
+      console.log(e.response.error,"the error", "error object",e)
+     }
+    }
+
+   return( <Page title =" Home " wide={true} >
     <div className="row align-items-center">
       <div className="col-lg-7 py-3 py-md-5">
         <h1 className="display-3">Remember Writing?</h1>
         <p className="lead text-muted">Are you sick of short tweets and impersonal &ldquo;shared&rdquo; posts that are reminiscent of the late 90&rsquo;s email forwards? We believe getting back to actually writing is the key to enjoying the internet again.</p>
       </div>
       <div className="col-lg-5 pl-lg-5 pb-3 py-lg-5">
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="form-group">
             <label htmlFor="username-register" className="text-muted mb-1">
               <small>Username</small>
             </label>
-            <input id="username-register" name="username" className="form-control" type="text" placeholder="Pick a username" autoComplete="off" />
+            <input onChange={e => setusername(e.target.value)} id="username-register" name="username" className="form-control" type="text" placeholder="Pick a username" autoComplete="off" />
           </div>
           <div className="form-group">
             <label htmlFor="email-register" className="text-muted mb-1">
               <small>Email</small>
             </label>
-            <input id="email-register" name="email" className="form-control" type="text" placeholder="you@example.com" autoComplete="off" />
+            <input id="email-register" name="email" 
+            onChange={e => setuseremail(e.target.value)}
+            className="form-control" type="text" placeholder="you@example.com" autoComplete="off" />
           </div>
           <div className="form-group">
             <label htmlFor="password-register" className="text-muted mb-1">
               <small>Password</small>
             </label>
-            <input id="password-register" name="password" className="form-control" type="password" placeholder="Create a password" />
+            <input id="password-register" name="password" 
+            onChange={e => setpassword(e.target.value)}
+            className="form-control" type="password" placeholder="Create a password" />
           </div>
           <button type="submit" className="py-3 mt-4 btn btn-lg btn-success btn-block">
             Sign up for ComplexApp
@@ -39,11 +83,11 @@ const Main = () => (
   </Page>
 
 
+   )
 
 
 
-
-)
+    }
 
 
 export default Main
